1bd938f3a1a7ff745f8417ce8fffa4ce5358d2b4
Matthew Hughes <matthew.hughes71499@gmail.com>
2020-04-05 09:27:37 -0700

    Updated bitAdder to handle bit addition with different sized lists, and added some helper functions from converting bitlists to decimal and vice versa

--- main.hs
+++ main.hs
@@ -1,3 +1,19 @@
+-- These helper functions are just to make verifying calculations quicker and easier
+greatestPowerOfTwo :: Int -> (Int,Int)
+greatestPowerOfTwo n = let (Just x) = lookup 1 powers in x
+  where powers = map (\x -> (n `div` 2^x, (2^x, x))) [0..]
+
+decToBits :: Int -> [Bool]
+decToBits 0 = [False]
+decToBits x = addBits ([True] ++ replicate (p) False) (decToBits y)
+  where (gp, p) = greatestPowerOfTwo x
+        y = x - gp
+
+bitsToDec :: [Bool] -> Int
+bitsToDec [] = 0
+bitsToDec (x:xs) = if x then 2 ^ (length xs) + bitsToDec xs else bitsToDec xs
+-- End helpers
+
 xor :: Bool -> Bool -> Bool
 xor x y = x /= y
 
@@ -16,42 +32,21 @@ fullAdder x y z =
       [xy3, xy4] = halfAdder xy2 z
   in  [xy3 || xy1, xy4]
 
--- NOTE: Leaving these in for now to reference how fullAdders are chained together
---doubleFullAdder :: [Bool] -> [Bool]
---doubleFullAdder [x1, y1, z, x2, y2] = 
---  let [c, s] = fullAdder x1 y1 z
---      [c2, s2] = fullAdder x2 y2 c
---  in  [c2, s2, s]
---
---tripleFullAdder :: [Bool] -> [Bool]
---tripleFullAdder [x1, y1, z, x2, y2, x3, y3] =
---  let [c, s2, s] = doubleFullAdder [x1, y1, z, x2, y2]
---      [c2, s3] = fullAdder x3 y3 c
---  in  [c2, s3, s2, s]
-
--- TODO: Figure out the best way to convert a decimal number to a list of Bools
---decToBits :: Int -> [Bool]
---decToBits x = 
---
---greatestPowerOfTwo :: Int -> Int
---greatestPowerOfTwo x = greatestPower x 2
---
---greatestPower :: Int -> Int -> Int
---greatestPower 0 y = y `div` 2
---greatestPower 1 y = y
---greatestPower 2 y = y
---greatestPower x y = greatestPower (x `div` 2) (y*2)
-
 -- NOTE: Reversing lists here to allow params to be passed in
--- left justified since that's how numbers are typically represented
+-- left justified
 -- EX: [True, False] is processed as 10, not 01
 addBits :: [Bool] -> [Bool] -> [Bool]
 addBits x y = bitAdder (reverse x) (reverse y) False
 
-
+-- TODO: Refactor
 bitAdder :: [Bool] -> [Bool] -> Bool -> [Bool]
-bitAdder [] _ c = [c]
-bitAdder _ [] c = [c]
+bitAdder [] [] c = [c]
+bitAdder [] (y:ys) c =
+  let [c1, s] = fullAdder False y c 
+  in bitAdder [] ys c1 ++ [s]
+bitAdder (x:xs) [] c =
+  let [c1, s] = fullAdder x False c 
+  in bitAdder xs [] c1 ++ [s]
 bitAdder (x:xs) (y:ys) c = 
   let [c1, s] = fullAdder x y c 
   in bitAdder xs ys c1 ++ [s]
